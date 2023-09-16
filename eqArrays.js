d08657e97fff8f77755e0d794447e033232d3412
Faduma Diriye <97137981+Faduma9@users.noreply.github.com>
2023-09-15 23:26:14 -0400

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,6 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected){
-  if(actual === expected) {
+const assertArraysEqual = function(actual, expected){
+  if (eqArrays(actual === expected)) {
         console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
       } else {
         console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
@@ -15,9 +15,9 @@ eqArrays([1, 2, 3], [3, 2, 1]) // => false
 eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // Should pass
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // Should pass
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // Should pass
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // Should pass
-assertEqual(eqArrays([], []), true); // Should pass for empty arrays
 
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // Should pass
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // Should fail
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // Should pass
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // Should fail
+assertArraysEqual([], []); // Should pass for empty arrays
