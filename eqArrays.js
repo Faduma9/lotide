a2fbed7ee93cd71012ebdd53e20173ec07a30e46
Faduma Diriye <97137981+Faduma9@users.noreply.github.com>
2023-09-16 17:38:12 -0400

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,23 +1,37 @@
-// FUNCTION IMPLEMENTATION
-const assertArraysEqual = function(actual, expected){
-  if (eqArrays(actual === expected)) {
-        console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-      } else {
-        console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-      }
+/ FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
+//cmpare two arrays equality
+//check if array have different length, if they ahve diff length return false
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  //loop through the element
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
    
 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3])); // Should pass
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1])); // Should fail
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // Should pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])); // Should fail
+assertEqual(eqArrays([], [])); // Should pass for empty arrays
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // Should pass
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // Should fail
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // Should pass
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // Should fail
-assertArraysEqual([], []); // Should pass for empty arrays
