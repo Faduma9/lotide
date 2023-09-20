68093c1feae583daae28284190c8f92799d7fce1
Faduma Diriye <97137981+Faduma9@users.noreply.github.com>
2023-09-20 14:07:52 -0400

    Update tail.js

--- tail.js
+++ tail.js
@@ -1,6 +1,5 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -8,23 +7,33 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-
-[1, 2, 3] === [1, 2, 3] // => false
-[1, 2, 3] == [1, 2, 3] // => false
-
-
-const result  = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
+// FUNCTION IMPLEMENTATION
+const tail = function(array) {
+  // Check if the array has at least one element
+  if (array.length <= 1) {
+    return [];
+  }
+  // Return a new array containing all elements except the first one
+  return array.slice(1);
+};
 
 // Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure the first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure the second element is "Labs"
 
-  const result = tail(["Hello", "Lighthouse", "Labs"]);
-  assertEqual(result.length, 2); // ensure we get back two elements
-  assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-  assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// Test Case 2: Check an array with only one element
+const singleElementArray = ["OnlyOne"];
+const singleElementResult = tail(singleElementArray);
+assertEqual(singleElementResult.length, 0); // ensure we get an empty array
 
-  // Test Case: Check the original array 
+// Test Case 3: Check an empty array
+const emptyArray = [];
+const emptyArrayResult = tail(emptyArray);
+assertEqual(emptyArrayResult.length, 0); // ensure we get an empty array
+
+// Test Case 4: Check the original array (should not be modified)
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
+tail(words);
 assertEqual(words.length, 3); // original array should still have 3 elements!
