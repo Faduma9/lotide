bab583a203614149e9bf618dbbe614b1582c8b87
Faduma <aladirie@gmail.com>
2023-09-20 15:11:20 -0400

    refactoreqArrays

---
+++ test/eqArraysTest.js
@@ -0,0 +1,11 @@
+//test/tailTest.js
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+
+// Test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // Should pass
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // Should fail
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // Should pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // Should fail
+assertEqual(eqArrays([], []), true); // Should pass
