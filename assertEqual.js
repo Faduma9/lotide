463648c89c71b7567cd9bfa9da9ebf19722454ef
Faduma <aladirie@gmail.com>
2023-09-20 13:16:49 -0400

    head and asserEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,35 +1,20 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-functionassertEqual (actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  }
-  
-
-  //Comparing identical strings
-  assertEqual("Lighthouse Labs", "Lighthouse Labs"); // Should pass
-//Comparing non-identical strings
+// Comparing identical strings
+assertEqual("Lighthouse Labs", "Lighthouse Labs"); // Should pass
+// Comparing non-identical strings
 assertEqual("Lighthouse Labs", "Bootcamp"); // Should fail
-//Comparing identical numbers
+// Comparing identical numbers
 assertEqual(1, 1); // Should pass
-//Comparing non-identical numbers
-assertEqual(1, 2); // Should fail 
+// Comparing non-identical numbers
+assertEqual(1, 2); // Should fail
 
-
-/*const name = 'Alice';
-console.log('Hello, ' + name + '!'); // logs: Hello, Alice!
-instead write it like this 
-The next example greets Alice using the template literals syntax.
-
-const name = 'Alice';
-console.log(`Hello, ${name}!`); // logs: Hello, Alice! /*
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,19 +1,13 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-  
+const assertEqual = require('./assertEqual');
 // Define the head function
 const head = function(arr) {
   if (arr.length === 0) {
     return undefined;
   }
   return arr[0];
+
 };
+module.exports = head;
   
 // Test array with multiple elements
 assertEqual(head([5, 6, 7]), 5);
