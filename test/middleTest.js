d666d73b5061bf3e8d58bb6e7fd38f0cd45c616e
Faduma <aladirie@gmail.com>
2023-09-26 01:51:31 -0400

    Refactor tailTest.js and middleTest.js to use Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,41 +1,20 @@
 const assert = require('chai').assert;
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
-
 describe('middle', () => {
-  it('should return an empty array for an input array with one element', () => {
-    const inputArray = [1];
-    const expected = [];
-    assert.deepEqual(middle(inputArray), expected);
+  it('should return an empty array for an array with less than 3 elements', () => {
+    const result = middle([1]);
+    assert.deepEqual(result, []);
   });
 
-  it('should return an empty array for an input array with two elements', () => {
-    const inputArray = [1, 2];
-    const expected = [];
-    assert.deepEqual(middle(inputArray), expected);
+  it('should return a single middle element for an array with odd length', () => {
+    const result = middle([1, 2, 3]);
+    assert.deepEqual(result, [2]);
   });
 
-  it('should return [2] for an input array [1, 2, 3]', () => {
-    const inputArray = [1, 2, 3];
-    const expected = [2];
-    assert.deepEqual(middle(inputArray), expected);
+  it('should return two middle elements for an array with even length', () => {
+    const result = middle([1, 2, 3, 4]);
+    assert.deepEqual(result, [2, 3]);
   });
 
-  it('should return [2, 3] for an input array [1, 2, 3, 4]', () => {
-    const inputArray = [1, 2, 3, 4];
-    const expected = [2, 3];
-    assert.deepEqual(middle(inputArray), expected);
-  });
-
-  it('should return [3] for an input array [1, 2, 3, 4, 5]', () => {
-    const inputArray = [1, 2, 3, 4, 5];
-    const expected = [3];
-    assert.deepEqual(middle(inputArray), expected);
-  });
-
-  it('should return [3, 4] for an input array [1, 2, 3, 4, 5, 6]', () => {
-    const inputArray = [1, 2, 3, 4, 5, 6];
-    const expected = [3, 4];
-    assert.deepEqual(middle(inputArray), expected);
-  });
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,30 +1,24 @@
 const assert = require('chai').assert;
 const tail = require('../tail');
+const assertArraysEqual = require('../assertArraysEqual');
 
 describe('tail', () => {
-  it('should return ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
-    const inputArray = ["Hello", "Lighthouse", "Labs"];
-    const expected = ["Lighthouse", "Labs"];
-    assert.deepEqual(tail(inputArray), expected);
+  it('should return an empty array for an array with only one element', () => {
+    const result = tail(["OnlyOne"]);
+    assert.deepEqual(result, []);
   });
 
-  it('should return an empty array for an input array with only one element', () => {
-    const singleElementArray = ["OnlyOne"];
-    const expected = [];
-    assert.deepEqual(tail(singleElementArray), expected);
+  it('should return an empty array for an empty array', () => {
+    const result = tail([]);
+    assert.deepEqual(result, []);
   });
 
-  it('should return an empty array for an empty input array', () => {
-    const emptyArray = [];
-    const expected = [];
-    assert.deepEqual(tail(emptyArray), expected);
+  it('should return the tail of an array with multiple elements', () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result, ["Lighthouse", "Labs"]);
   });
 
-  it('should not modify the original array ["Yo Yo", "Lighthouse", "Labs"]', () => {
-    const words = ["Yo Yo", "Lighthouse", "Labs"];
-    const originalLength = words.length;
-    tail(words);
-    assert.equal(words.length, originalLength);
-  });
 });
+
+
 
